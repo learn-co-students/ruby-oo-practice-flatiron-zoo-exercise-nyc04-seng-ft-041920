176a555686f1b4e58278b6f57d0f3a2a6d9fdb62
aleksarad <alexa.radevich1@gmail.com>
2020-05-17 14:02:22 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,24 @@
 class Animal
+    attr_reader :species, :nickname
+    attr_accessor :weight, :zoo
+
+    @@all = []
+
+    def initialize (species, weight, nickname, zoo)
+        @species = species 
+        @weight = weight 
+        @nickname = nickname
+        @zoo = zoo
+
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def self.find_by_species(species)
+        self.all.find_all {|animal| animal.species == species}
+    end
 
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,51 @@
+require_relative 'Animal.rb'
+
 class Zoo
+    attr_accessor :name, :location
+
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    # def new_animal(species, weight, nickname)
+    #     Animal.new(species, weight, nickname, self)
+    # end
+
+    def animals 
+        Animal.all.find_all {|animal| animal.zoo == self}
+    end
+
+    def animal_species
+        self.animals.collect {|animal| animal.species}.uniq
+    end
+
+    def find_by_species(species)
+        self.animals.find_all {|animal| animal.species == species}
+    end
+
+    def animal_nicknames
+        self.animals.collect {|animal| animal.nickname}
+    end
+
+    def self.find_by_location(location)
+        Zoo.all.find_all {|zoo| zoo.location == location}
+    end
 
 end
+
+# bronx = Zoo.new("Bronx Zoo", "New York")
+# brooklyn = Zoo.new("Brooklyn Zoo", "New York")
+# sanfran = Zoo.new("San Francisco Zoo", "San Francisco")
+
+# chester = Animal.new("Cheetah", 100, "Chester", bronx)
+# oreo = Animal.new("Panda", 200, "Oreo", bronx)
+# marble = Animal.new("Panda", 250, "Marble", bronx)
