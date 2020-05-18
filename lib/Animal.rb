39d00b8a12685a78596765266b9faa804e60b000
github username <github email address>
2020-05-18 16:17:57 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,23 @@
 class Animal
 
+    attr_reader :species, :nickname, :zoo
+    attr_accessor :weight
+
+    @@all = []
+
+    def initialize(species, weight, nickname, zoo)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @zoo = zoo
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species)
+        self.all.select{|animal| animal.species == species}
+    end
 end
--- run.rb
+++ run.rb
@@ -4,6 +4,22 @@ require 'pry'
 
 
 #Test your code here
+z1 = Zoo.new("Bronx Zoo", "Bronx, NY")
+z11 = Zoo.new("Other Bronx Zoo", "Bronx, NY")
+
+z2 = Zoo.new("Turtle Back Zoo", "West Orange, NJ")
+z3 = Zoo.new("San Diego Zoo", "San Diego, CA")
+
+a1 = Animal.new("frog", 0.3, "Jeremiah", z1)
+a2 = Animal.new("giraffe", 1000, "Jeffrey", z2)
+a3 = Animal.new("cow", 700, "Bessie", z2)
+a4 = Animal.new("horse", 500, "Nellie", z3)
+a5 = Animal.new("pig", 200, "Wilbur", z3)
+a6 = Animal.new("cow", 750, "Bertha", z2)
+a7 = Animal.new("pig", 250, "Snuffles", z1)
+
+
+
 
 
 binding.pry
