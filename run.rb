563bdcc8eca6241c2ed132660116be742a3f094d
frod25 <frod25>
2020-05-19 12:20:14 -0400

    Done.

--- run.rb
+++ run.rb
@@ -4,7 +4,15 @@ require 'pry'
 
 
 #Test your code here
+zoo1 = Zoo.new("name1", "location1")
+zoo2 = Zoo.new("name2", "location2")
+zoo3 = Zoo.new("name3", "location3")
 
+animal1 = Animal.new("species1", 50, "nickname1", zoo1)
+animal2 = Animal.new("species2", 55, "nickname2", zoo2)
+animal3 = Animal.new("species3", 45, "nickname3", zoo3)
+
+animal4 = Animal.new("species1", 58, "nickname4", zoo1)
 
 binding.pry
 puts "done"
