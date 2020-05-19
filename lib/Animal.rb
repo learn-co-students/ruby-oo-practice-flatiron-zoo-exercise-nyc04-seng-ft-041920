df0e4052afe04df629ac1183094e94eca52bfdf3
amer519 <afahmy519@gmail.com>
2020-05-19 00:45:59 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,27 @@
 class Animal
 
+    attr_reader :species, :nickname
+    attr_accessor :weight
+
+    @@all = []
+
+    def initialize(zoo, species, weight, nickname)
+        @zoo = zoo
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species)
+        Animal.all.select {|animal| animal.species == species}
+    end
+
+
+
+
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,49 @@
 class Zoo
 
+    
+    attr_reader :name, :location, :animals
+
+    @all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def animals
+        Animals.all.select {|animal| animal.zoo == self}
+    end
+
+    def animal_species
+        animals.map(&:species).uniq
+    end
+
+    def find_by_species(species)
+        animals.select{|animal| animal.species == species}
+    end
+
+    def animal_nickname
+        animals.map(&:animal.nickname)
+    end
+
+    def self.find_by_location(location)
+        Zoo.all.select {|zoo| zoo.location == location}
+    end
+
+
+
+
+
+
+
+
+    
+    
+    
+
 end
